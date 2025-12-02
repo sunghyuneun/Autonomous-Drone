2b8ebe1c42549dbc19142c6678a062394b4b5ab5
GregEun <gregoryeun@gmail.com>
2025-12-01 21:33:31 -0800

    Made rasppi i hope it works

---
+++ ROS2/rasppi.py
@@ -0,0 +1,31 @@
+import serial
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+SERIAL_PORT = '/dev/ttyACM0'
+BAUD_RATE = 9600
+ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout = 1)
+
+class RPiSubscriber(Node):
+    def __init__(self):
+        super().__init__('rpi_subscriber')
+        self.subscription = self.create_subscription(Int16,'input',self.arduino_write, 10)
+
+
+    def arduino_write(self, msg):
+        ser.write(int(msg).encode())
+        self.get_logger().info(f"Sent: {msg}")
+
+def main(args = None):
+    rclpy.init(args=args)
+
+    rpi_subscriber = RPiSubscriber()
+    rclpy.spin(rpi_subscriber)
+
+    rpi_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
