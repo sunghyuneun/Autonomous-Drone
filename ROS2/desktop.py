ddb204087d87a254d9b6c7e39dd2fbfdb1a77e33
GregEun <gregoryeun@gmail.com>
2025-12-01 20:27:05 -0800

    Fixed ROS2 publisher node

--- ROS2/desktop.py
+++ ROS2/desktop.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-
+import threading
 from std_msgs.msg import Int16
 
 class DesktopNode(Node):
@@ -8,20 +8,43 @@ class DesktopNode(Node):
     def __init__(self):
         super().__init__('desktop_node')
         self.publisher_ = self.create_publisher(Int16, 'input', 10)
+        self.get_logger().info("Created input publisher")
 
-    def publish_msg(self):
-        msg = Int16()
-        msg.data = int(input('Input number (0-10)'))
-        self.publisher_.publish(msg)
+        self.input_thread = threading.Thread(target=self.publish_input)
+        self.input_thread.daemon = True
+        self.input_thread.start()
+        self.get_logger().info("Input thread started.")
+
+    def publish_input(self):
+        while rclpy.ok():
+            try:
+                user_input = input('Input number (0-10)')
+                if (not user_input.strip()) and user_input.strip().isdigit():
+                    self.get_logger().warn("Wrong input format")
+                    continue
+
+                user_num = int(user_input)
+
+                if 0 <= user_num <= 10:
+                    msg = Int16()
+                    msg.data = user_num
+                    self.publisher_.publish(msg)
+                    self.get_logger().info(f"Published: {msg.data}")
+            except Exception as e:
+                self.get_logger().warn(f"Ran into input exception: {e}")
+                break
 
 
 def main(args=None):
     rclpy.init(args=args)
     desktop_node = DesktopNode()
-    rclpy.spin(desktop_node)
-
-    desktop_node.destroy_node()
-    rclpy.shutdown()
+    try:
+        rclpy.spin(desktop_node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        desktop_node.destroy_node()
+        rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
