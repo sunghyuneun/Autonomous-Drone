ebf91e882f0e72313e8ecc133b03c2055a8f3dd1
Sung Hyun Eun <33554699+GregEun@users.noreply.github.com>
2025-12-28 09:46:55 -0800

    Created PID control for the bot

--- ArduinoCode/src/main.cpp
+++ ArduinoCode/src/main.cpp
@@ -1,5 +1,3 @@
-//Stuff I gotta do: PID control w/ target vel and actual vel
-
 #define ENCODER_OPTIMIZE_INTERRUPTS
 
 #include <Arduino.h>
@@ -61,8 +59,25 @@ Adafruit_MPU6050 mpu;
 Encoder encoder1(HALL1A,HALL1B);
 Encoder encoder2(HALL2A,HALL2B);
 
+float vel_1 = 0.0;
+float vel_2 = 0.0;
+
+float kp = 150.0;
+float ki = 40.0;
+float kd = 1.0;
+
+
+float target_vel_1 = 0.0;
+float target_vel_2 = 0.0;
+float integral1 = 0.0;
+float integral2 = 0.0;
+float last_error_1 = 0.0;
+float last_error_2 = 0.0;
+
+
 void IMU_Calibration();
 void print_IMU();
+void drive_motor(float, float);
 
 void setup() {
   pinMode(HALL1A, INPUT);
@@ -100,27 +115,33 @@ void setup() {
 
 void loop() {
   unsigned long current_time = millis();
-  //Gets new sensor events with readings
+
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  //Give the Sensor stuff!
-  
-  digitalWrite(PH1, LOW);
-  digitalWrite(PH2, HIGH);
-  analogWrite(EN1, 50);
-  analogWrite(EN2, 50);
-  //Serial.println(encoder1.read());
+  if (Serial.available() > 0){
+    char key = Serial.read();
+    if (key == 'R'){
+      target_vel_1 = -1 * Serial.parseFloat();
+      Serial.print("R: ");
+      Serial.println(target_vel_1);
+    }
+    else if (key == 'L'){
+      target_vel_2 = Serial.parseFloat();
+      Serial.print("L: ");
+      Serial.println(target_vel_2);
+    }
+  }
 
   if ((current_time - last_time) >= interval){
     current_pos1 = encoder1.read();
     current_pos2 = encoder2.read();
 
     float time_delta = (current_time - last_time) / 1000.0;
-    float vel_1 = (current_pos1 - last_pos1) * METERS_PER_TICK / time_delta;
-    float vel_2 = (current_pos2 - last_pos2) * METERS_PER_TICK / time_delta;
+    vel_1 = (current_pos1 - last_pos1) * METERS_PER_TICK / time_delta;
+    vel_2 = (current_pos2 - last_pos2) * METERS_PER_TICK / time_delta;
     Serial.print("V:");
-    Serial.print(vel_1);
+    Serial.print(-1 * vel_1);
     Serial.print(",");
     Serial.print(vel_2);
     Serial.print(",");
@@ -142,6 +163,21 @@ void loop() {
     Serial.print(",");
     Serial.print(g.gyro.z - Z_GYRO_BIAS);
     Serial.print("\n");
+
+    float error1 = target_vel_1 - vel_1;
+    float error2 = target_vel_2 - vel_2;
+    integral1 += error1 * time_delta;
+    integral1 = constrain(integral1, -100, 100);
+    integral2 += error2 * time_delta;
+    integral2 = constrain(integral2, -100, 100);
+    float derivative1 = (error1 - last_error_1) / time_delta;
+    float derivative2 = (error2 - last_error_2) / time_delta;
+    float output1 = (kp * error1) + (ki * integral1) + (kd * derivative1);
+    float output2 = (kp * error2) + (ki * integral2) + (kd * derivative2);
+
+    drive_motor(output1, output2);
+    last_error_1 = error1;
+    last_error_2 = error2;
   }
 }
 
@@ -186,4 +222,17 @@ void IMU_Calibration(){
   Serial.print(",");
   Serial.print(Z_GYRO_BIAS);
   Serial.print("\n");
+}
+
+
+void drive_motor(float power1, float power2){
+  if (power1 >= 0) digitalWrite(PH1, HIGH);
+  else digitalWrite(PH1, LOW);
+  if (power2 >= 0) digitalWrite(PH2, HIGH);
+  else digitalWrite(PH2, LOW);
+  int pwmVal1 = constrain(abs(power1), 0, 255);
+  int pwmVal2 = constrain(abs(power2), 0, 255);
+  //Max is like 1.64 lol
+  analogWrite(EN1, pwmVal1);
+  analogWrite(EN2, pwmVal2);
 }
--- DC Motor Test/src/main.cpp
+++ DC Motor Test/src/main.cpp
@@ -32,6 +32,21 @@ const int interval = 50;
 Encoder encoder1(HALL1A,HALL1B);
 Encoder encoder2(HALL2A,HALL2B);
 
+float vel_1 = 0.0;
+float vel_2 = 0.0;
+
+float kp = 150.0;
+float ki = 40.0;
+float kd = 1.0;
+
+
+float target_vel_1 = 0.0;
+float target_vel_2 = 0.0;
+float integral1 = 0.0;
+float integral2 = 0.0;
+float last_error_1 = 0.0;
+float last_error_2 = 0.0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(PH1, OUTPUT);
@@ -49,20 +64,69 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(PH1, HIGH);
-  digitalWrite(PH2, LOW);
-  analogWrite(EN1, 50);
-  analogWrite(EN2, 50);
-  delay(1000);
-  digitalWrite(PH1, LOW);
-  digitalWrite(PH2, HIGH);
-  analogWrite(EN1, 50);
-  analogWrite(EN2, 50);
-  delay(1000);
-  // put your main code here, to run repeatedly:
+  unsigned long current_time = millis();
+  
+  if (Serial.available() > 0){
+    char key = Serial.read();
+    if (key == 'R'){
+      target_vel_1 = -1 * Serial.parseFloat();
+      Serial.print("R: ");
+      Serial.println(target_vel_1);
+    }
+    else if (key == 'L'){
+      target_vel_2 = Serial.parseFloat();
+      Serial.print("L: ");
+      Serial.println(target_vel_2);
+    }
+  }
+  
+  if ((current_time - last_time) >= interval){
+    
+    current_pos1 = encoder1.read();
+    current_pos2 = encoder2.read();
+
+    float time_delta = (current_time - last_time) / 1000.0;
+    
+    vel_1 = (current_pos1 - last_pos1) * METERS_PER_TICK / time_delta;
+    vel_2 = (current_pos2 - last_pos2) * METERS_PER_TICK / time_delta;
+  
+    
+    Serial.print("V:");
+    Serial.print(-1 * vel_1);
+    Serial.print(",");
+    Serial.print(vel_2);
+    Serial.print("\n");
+
+
+    last_pos1 = current_pos1;
+    last_pos2 = current_pos2;
+    last_time = current_time;
+
+    float error1 = target_vel_1 - vel_1;
+    float error2 = target_vel_2 - vel_2;
+    integral1 += error1 * time_delta;
+    integral1 = constrain(integral1, -100, 100);
+    integral2 += error2 * time_delta;
+    integral2 = constrain(integral2, -100, 100);
+    float derivative1 = (error1 - last_error_1) / time_delta;
+    float derivative2 = (error2 - last_error_2) / time_delta;
+    float output1 = (kp * error1) + (ki * integral1) + (kd * derivative1);
+    float output2 = (kp * error2) + (ki * integral2) + (kd * derivative2);
+
+    driveMotor(output1, output2);
+    last_error_1 = error1;
+    last_error_2 = error2;
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void driveMotor(float power1, float power2){
+  if (power1 >= 0) digitalWrite(PH1, HIGH);
+  else digitalWrite(PH1, LOW);
+  if (power2 >= 0) digitalWrite(PH2, HIGH);
+  else digitalWrite(PH2, LOW);
+  int pwmVal1 = constrain(abs(power1), 0, 255);
+  int pwmVal2 = constrain(abs(power2), 0, 255);
+  analogWrite(EN1, pwmVal1);
+  analogWrite(EN2, pwmVal2);
 }
