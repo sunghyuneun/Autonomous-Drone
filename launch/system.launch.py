2c51485fbd7aa1e14c8f68ba9a042d8947091ffb
Sung Hyun Eun <gregoryeun@gmail.com>
2026-01-03 20:23:19 -0800

    Whatever push this let's try it out lol

--- launch/system.launch.py
+++ launch/system.launch.py
@@ -1,24 +1,33 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    urdf_file = "../CAD/robot.urdf"
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    urdf_file = "~/Autonomous-Drone/CAD/robot.urdf"
     with open(urdf_file, 'r') as infp:
         robot_desc = infp.read()
         
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
         Node(
-            package='idk what this package is',
-            executable='arduino_listener.py',
-            name="arduino_listener",
-            output="screen"
-        ),
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+            arguments=[urdf_file]),
         Node(
-            package='idk what this package is either',
-            executable='the executable',
-            name="a name",
-            output="yeah lol"
-        )
+            package='urdf_tutorial_r2d2',
+            executable='state_publisher',
+            name='state_publisher',
+            output='screen'),
     ])
